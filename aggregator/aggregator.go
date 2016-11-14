84f56f5c26fecf1cc875e8178c9bc0b7d7eb606b
Ken Grønnbeck <kengroenn@gmail.com>
2016-11-14 20:06:17 +0100

    Enable CORS on aggregator

--- aggregator/aggregator.go
+++ aggregator/aggregator.go
@@ -79,6 +79,17 @@ func main() {
 	}()
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if origin := r.Header.Get("Origin"); origin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+			w.Header().Set("Access-Control-Allow-Headers",
+				"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		}
+		// Stop here if its Preflighted OPTIONS request
+		if r.Method == "OPTIONS" {
+			return
+		}
+
 		lock.RLock()
 		defer lock.RUnlock()
 
@@ -88,6 +99,7 @@ func main() {
 			log.Fatal(err)
 		}
 
+		w.Header().Set("content-type", "application/json")
 		w.Write(byt)
 	})
 
